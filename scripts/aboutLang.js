c2c06302d8e5887ddac0bfb19077a1e6ba8c3c4a
Vass Bálint <vassbalint6366@gmail.com>
2023-07-28 18:09:15 +0200

    Update about.html with new information and improve styling

--- scripts/aboutLang.js
+++ scripts/aboutLang.js
@@ -7,6 +7,25 @@ function changeAboutLanguage() {
     var aboutACSC = document.getElementById('aboutACSC');
     var aboutHighSchool = document.getElementById('aboutHighSchool');
     var aboutSkills = document.getElementById('aboutSkills');
+    var aboutSkillsIT = document.getElementById('aboutSkillsIT');
+    var aboutSkillsSoftwares = document.getElementById('aboutSkillsSoftwares');
+    var aboutSkillsPL = document.getElementById('aboutSkillsPL');
+    var aboutSkillsTesting = document.getElementById('aboutSkillsTesting');
+    var aboutSkillsInterpersonal = document.getElementById('aboutSkillsInterpersonal');
+
+    var aboutSkillsCommunication = document.getElementById('aboutSkillsCommunication');
+    var aboutSkillsCommunicationContent = document.getElementById('aboutSkillsCommunicationContent');
+    var aboutSkillsProblemSolving = document.getElementById('aboutSkillsProblemSolving');
+    var aboutSkillsProblemSolvingContent = document.getElementById('aboutSkillsProblemSolvingContent');
+    var aboutSkillsEmpathy = document.getElementById('aboutSkillsEmpathy');
+    var aboutSkillsEmpathyContent = document.getElementById('aboutSkillsEmpathyContent');
+    var aboutSkillsTeamwork = document.getElementById('aboutSkillsTeamwork');
+    var aboutSkillsTeamworkContent = document.getElementById('aboutSkillsTeamworkContent');
+    var aboutSkillsTimeManagment = document.getElementById('aboutSkillsTimeManagment');
+    var aboutSkillsTimeManagmentContent = document.getElementById('aboutSkillsTimeManagmentContent');
+    var aboutSkillsConflict = document.getElementById('aboutSkillsConflict');
+    var aboutSkillsConflictContent = document.getElementById('aboutSkillsConflictContent');
+
     var aboutLanguages = document.getElementById('aboutLanguages');
     var aboutLanguagesHU = document.getElementById('aboutLanguagesHU');
     var aboutLanguagesEN = document.getElementById('aboutLanguagesEN');
@@ -22,6 +41,25 @@ function changeAboutLanguage() {
         aboutACSC.innerHTML = englishContent.aboutACSC;
         aboutHighSchool.innerHTML = englishContent.aboutHighSchool;
         aboutSkills.innerHTML = englishContent.aboutSkills;
+        aboutSkillsIT.innerHTML = englishContent.aboutSkillsIT;
+        aboutSkillsSoftwares.innerHTML = englishContent.aboutSkillsSoftwares;
+        aboutSkillsPL.innerHTML = englishContent.aboutSkillsPL;
+        aboutSkillsTesting.innerHTML = englishContent.aboutSkillsTesting;
+        aboutSkillsInterpersonal.innerHTML = englishContent.aboutSkillsInterpersonal;
+
+        aboutSkillsCommunication.innerHTML = englishContent.aboutSkillsCommunication;
+        aboutSkillsCommunicationContent.innerHTML = englishContent.aboutSkillsCommunicationContent;
+        aboutSkillsProblemSolving.innerHTML = englishContent.aboutSkillsProblemSolving;
+        aboutSkillsProblemSolvingContent.innerHTML = englishContent.aboutSkillsProblemSolvingContent;
+        aboutSkillsEmpathy.innerHTML = englishContent.aboutSkillsEmpathy;
+        aboutSkillsEmpathyContent.innerHTML = englishContent.aboutSkillsEmpathyContent;
+        aboutSkillsTeamwork.innerHTML = englishContent.aboutSkillsTeamwork;
+        aboutSkillsTeamworkContent.innerHTML = englishContent.aboutSkillsTeamworkContent;
+        aboutSkillsTimeManagment.innerHTML = englishContent.aboutSkillsTimeManagment;
+        aboutSkillsTimeManagmentContent.innerHTML = englishContent.aboutSkillsTimeManagmentContent;
+        aboutSkillsConflict.innerHTML = englishContent.aboutSkillsConflict;
+        aboutSkillsConflictContent.innerHTML = englishContent.aboutSkillsConflictContent;
+
         aboutLanguages.innerHTML = englishContent.aboutLanguages;
         aboutLanguagesHU.innerHTML = englishContent.aboutLanguagesHU;
         aboutLanguagesEN.innerHTML = englishContent.aboutLanguagesEN;
@@ -36,6 +74,25 @@ function changeAboutLanguage() {
         aboutACSC.innerHTML = hungarianContent.aboutACSC;
         aboutHighSchool.innerHTML = hungarianContent.aboutHighSchool;
         aboutSkills.innerHTML = hungarianContent.aboutSkills;
+        aboutSkillsIT.innerHTML = hungarianContent.aboutSkillsIT;
+        aboutSkillsSoftwares.innerHTML = hungarianContent.aboutSkillsSoftwares;
+        aboutSkillsPL.innerHTML = hungarianContent.aboutSkillsPL;
+        aboutSkillsTesting.innerHTML = hungarianContent.aboutSkillsTesting;
+        aboutSkillsInterpersonal.innerHTML = hungarianContent.aboutSkillsInterpersonal;
+
+        aboutSkillsCommunication.innerHTML = hungarianContent.aboutSkillsCommunication;
+        aboutSkillsCommunicationContent.innerHTML = hungarianContent.aboutSkillsCommunicationContent;
+        aboutSkillsProblemSolving.innerHTML = hungarianContent.aboutSkillsProblemSolving;
+        aboutSkillsProblemSolvingContent.innerHTML = hungarianContent.aboutSkillsProblemSolvingContent;
+        aboutSkillsEmpathy.innerHTML = hungarianContent.aboutSkillsEmpathy;
+        aboutSkillsEmpathyContent.innerHTML = hungarianContent.aboutSkillsEmpathyContent;
+        aboutSkillsTeamwork.innerHTML = hungarianContent.aboutSkillsTeamwork;
+        aboutSkillsTeamworkContent.innerHTML = hungarianContent.aboutSkillsTeamworkContent;
+        aboutSkillsTimeManagment.innerHTML = hungarianContent.aboutSkillsTimeManagment;
+        aboutSkillsTimeManagmentContent.innerHTML = hungarianContent.aboutSkillsTimeManagmentContent;
+        aboutSkillsConflict.innerHTML = hungarianContent.aboutSkillsConflict;
+        aboutSkillsConflictContent.innerHTML = hungarianContent.aboutSkillsConflictContent;
+
         aboutLanguages.innerHTML = hungarianContent.aboutLanguages;
         aboutLanguagesHU.innerHTML = hungarianContent.aboutLanguagesHU;
         aboutLanguagesEN.innerHTML = hungarianContent.aboutLanguagesEN;
